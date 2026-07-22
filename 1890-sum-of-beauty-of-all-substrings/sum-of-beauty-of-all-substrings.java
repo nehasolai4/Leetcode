eb013ef990958ca20edac6974f98bbf241ac9861
Neha S <nehasolai@gmail.com>
2026-07-22 21:28:13 +0530

    Time: 58 ms (94.23%) | Memory: 44.1 MB (72.45%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -2,14 +2,14 @@ class Solution {
     public int beautySum(String s) {
         int result=0;
         for(int i=0;i<s.length();i++){
+            int arr[] = new int[26];
             for(int j=i;j<s.length();j++){
-                int arr[] = new int[26];
+
                 int min=501;
                 int max=0;
-                for(int k=i;k<=j;k++){
-                    char ch = s.charAt(k);
-                    arr[ch-'a']++;
-                }
+                
+                char ch = s.charAt(j);
+                arr[ch-'a']++;                
 
                 for(int k=0;k<26;k++){
                     if(arr[k]>0){
@@ -18,7 +18,6 @@ class Solution {
                     }
                 }
                 result+=(max-min);
-
             }
         }
         return result;
