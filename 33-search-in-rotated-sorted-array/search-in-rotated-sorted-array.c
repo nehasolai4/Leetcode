a46695ea917f730cc6ea02e924c6b932f152b5f5
Neha S <nehasolai@gmail.com>
2026-07-30 00:30:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (69.02%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.c
@@ -0,0 +1,25 @@
+int search(int* nums, int numsSize, int target) {
+    
+    int left=0;
+    int right=numsSize-1;
+
+    while(left<=right){
+        int mid = left+(right-left)/2;
+
+        if(nums[mid]==target)
+            return mid;
+        else if(nums[left]<=nums[mid]){
+            if(nums[left]<=target && nums[mid]>target)
+                right=mid-1;
+            else
+                left=mid+1;
+        }
+        else if(nums[mid]<=nums[right]){
+            if(nums[mid]<target && nums[right]>=target)
+                left=mid+1;
+            else
+                right=mid-1;
+        }
+    }
+    return -1;
+}
