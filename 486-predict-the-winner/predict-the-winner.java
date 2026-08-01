aa97b6b8069e19c98ae5ee51943cf250875f19e2
Neha S <nehasolai@gmail.com>
2026-08-01 16:33:22 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (23.56%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,20 +1,27 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
     
-        
-        int diff = predict(nums,0,nums.length-1);
+        Integer dp[][] = new Integer[nums.length][nums.length];
+
+
+        int diff = predict(nums,0,nums.length-1,dp);
         return diff>=0;
         
     }
-    public int predict(int nums[],int left,int right){
+    public int predict(int nums[],int left,int right,Integer dp[][]){
+
+        if(dp[left][right]!=null)
+            return dp[left][right];
 
         if(left==right)
             return nums[left];
 
-        int takeLeft = nums[left]-predict(nums,left+1,right);
+        int takeLeft = nums[left]-predict(nums,left+1,right,dp);
 
-        int takeRight = nums[right]-predict(nums,left,right-1);
+        int takeRight = nums[right]-predict(nums,left,right-1,dp);
 
-        return Math.max(takeLeft,takeRight);
+        dp[left][right]= Math.max(takeLeft,takeRight);
+
+        return dp[left][right];
     }
 }
