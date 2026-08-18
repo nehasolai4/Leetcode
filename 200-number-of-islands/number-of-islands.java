a116244f818b3e361102e33be85a6eb7b993b05b
Neha S <nehasolai@gmail.com>
2026-08-18 10:56:27 +0530

    Time: 5 ms (28.52%) | Memory: 54.3 MB (9.61%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -12,35 +12,29 @@ class Solution {
             for(int j=0;j<c;j++){
                 if(grid[i][j]=='1' && !visited[i][j]){
                     islands++;
-
-                    Queue<int[]> queue = new LinkedList<>();
-                    visited[i][j]=true;
-
-                    queue.offer(new int[]{i,j});
-
-                    while(!queue.isEmpty()){
-                        int cell[]=queue.poll();
-
-                        int row = cell[0]; int col=cell[1];
-
-                        int directions[][]={{-1,0},{1,0},{0,-1},{0,1}};
-
-                        for(int dir[]:directions){
-                            int nrow = row+dir[0];
-                            int ncol = col+dir[1];
-
-                            if(nrow>=0 && nrow<r
-                            && ncol>=0 && ncol<c 
-                            && grid[nrow][ncol]=='1' && !visited[nrow][ncol]){
-                                visited[nrow][ncol]=true;
-                                queue.offer(new int[]{nrow,ncol});
-                            }
-                        }
-                    }
+                    dfs(grid,visited,i,j);
 
                 }
             }
         }
         return islands;
+    }
+
+    public void dfs(char grid[][],boolean visited[][],int row,int col){
+        visited[row][col]=true;
+
+        int directions[][] ={{-1,0},{1,0},{0,-1},{0,1}};
+
+        for(int dir[]:directions){
+            int nrow = row+dir[0];
+            int ncol = col+dir[1];
+
+            if(nrow>=0 && nrow<grid.length
+            && ncol>=0 && ncol<grid[0].length
+            && grid[nrow][ncol]=='1' && !visited[nrow][ncol]){
+
+                dfs(grid,visited,nrow,ncol);
+            }
+        }
     }
 }
