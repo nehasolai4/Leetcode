2b40a89422da5542d3c43c74e8c674cb2b3505e9
Neha S <nehasolai@gmail.com>
2026-08-20 13:03:20 +0530

    Time: 4 ms (100.00%) | Memory: 61.6 MB (48.09%) - LeetSync

---
+++ 1659-get-the-maximum-score/get-the-maximum-score.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int maxSum(int[] nums1, int[] nums2) {
+        int i=0;
+        int j=0;
+
+        long sum1=0;
+        long sum2=0;
+
+        long result=0;
+
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]<nums2[j]){
+                sum1+=nums1[i];
+                i++;
+            }
+            else if(nums1[i]>nums2[j]){
+                sum2+=nums2[j];
+                j++;
+            }
+            else{
+                result+= Math.max(sum1,sum2)+nums1[i];
+                i++;
+                j++;
+                sum1=0;
+                sum2=0;
+            }
+        }
+
+        while(i<nums1.length){
+            sum1+=nums1[i];
+            i++;
+        }
+
+        while(j<nums2.length){
+            sum2+=nums2[j];
+            j++;
+        }
+
+        result+=Math.max(sum1,sum2);
+        return (int)(result%1_000_000_007);
+
+    }
+}
