fdcfd4ad80c82301a1b43d40951e9f3114c672ca
Neha S <nehasolai@gmail.com>
2026-09-08 09:16:29 +0530

    Time: 1 ms (83.65%) | Memory: 42.8 MB (22.48%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -4,11 +4,7 @@ class Solution {
         if(n<1000)
             return 0;
 
-        int result=0;
-        for(int i=1000;i<=n;i++){
-            result++;
-        }
         
-        return result;
+        return (n-1000)+1;
     }
 }
