30b52e2ddb270efba503777d10bc23771030df89
Neha S <nehasolai@gmail.com>
2026-09-13 19:06:48 +0530

    Time: 5 ms (21.74%) | Memory: 48.5 MB (69.71%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] countBits(int n) {
+        
+        int result[] = new int[n+1];
+        for(int i=0;i<=n;i++){
+
+            int num = i;
+            int count=0;
+            while(num>0){
+                count+=num&1;
+                num=num>>1;
+            }
+
+            result[i]=count;
+        }
+
+        return result;
+    }
+}
